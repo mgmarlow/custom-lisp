d26c912c66e51c344b6f1a892a2924ef61b6b67f
Graham Marlow <mgmarlow@gmail.com>
2017-01-28 16:48:57 -0800

    Add initial parsing logic

--- src/repl.c
+++ src/repl.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "../lib/mpc.h"
+
 #ifdef _WIN32
 #include <string.h>
 
@@ -24,6 +26,21 @@ void add_history (char* unused) {}
 #endif
 
 int main (int argc, char** argv) {
+  mpc_parser_t* Number   = mpc_new("number");
+  mpc_parser_t* Operator = mpc_new("operator");
+  mpc_parser_t* Expr     = mpc_new("expr");
+  mpc_parser_t* Lispy    = mpc_new("lispy");
+
+  mpca_lang(MPCA_LANG_DEFAULT,
+    "                                                    \
+      number   : /-?[0-9]+/ ;                            \
+      operator : '+' | '-' | '*' | '/' ;                 \
+      expr     : <number> | '(' <operator> <expr>+ ')' ; \
+      lispy    : /^/ <operator> <expr>+ /$/ ;            \
+    ",
+    Number, Operator, Expr, Lispy
+  );
+
   puts("CLisp v0.1");
   puts("Press Ctrl+c to Exit");
 
@@ -31,9 +48,23 @@ int main (int argc, char** argv) {
     char* input = readline("clisp> "); 
     add_history(input);
 
+    // Parse user input
+    mpc_result_t r;
+    if (mpc_parse("<stdin>", input, Lispy, &r)) {
+      // Print AST
+      mpc_ast_print(r.output);
+      mpc_ast_delete(r.output);
+    } else {
+      // Print Error
+      mpc_err_print(r.error);
+      mpc_err_delete(r.error);
+    }
+
     printf("%s", input);
     free(input);
   }
 
+  // Undefine & delete parsers
+  mpc_cleanup(4, Number, Operator, Expr, Lispy);
   return 0;
 }
